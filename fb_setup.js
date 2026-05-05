1f4c0e21f2d3f2f52181d8e0cc44c2e4c8af74fb
Aaron Shaji <23353as@hvhs.school.nz>
2026-05-05 12:30:12 +1200

    Got inputs and connected it to database

---
+++ fb_setup.js
@@ -0,0 +1,13 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyDoc6oIXoHAGvwK-8MUV-Q4tbx_wvK6UdA",
+    authDomain: "aaron-shaji-12comp.firebaseapp.com",
+    databaseURL: "https://aaron-shaji-12comp-default-rtdb.firebaseio.com",
+    projectId: "aaron-shaji-12comp",
+    storageBucket: "aaron-shaji-12comp.firebasestorage.app",
+    messagingSenderId: "461472100432",
+    appId: "1:461472100432:web:a8646dd71cda8238ee6155",
+    measurementId: "G-9G30RS1KYT"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- script.js
+++ script.js
@@ -1,7 +1,96 @@
+console.log("Running Sal's Strawberries");
 
-console.log("Running Sal's Strawberries")
+// -----------------------------------
+// Form Function
+// -----------------------------------
 
-function writeForm(){
-    // Get the form data
-    const favoriteFruit = document.getElementById("favoriteFruit").value;
+function writeForm() {
+
+  const name =
+    document.getElementById("name").value;
+
+  const favoriteFruit =
+    document.getElementById("favoriteFruit").value;
+
+  const fruitQuantity =
+    document.getElementById("fruitQuantity").value;
+
+  // Get logged-in user
+  let user = firebase.auth().currentUser;
+
+  // Make sure user is logged in
+  if (!user) {
+
+    alert("Please log in first.");
+    return;
+
+  }
+
+  if (!name || !favoriteFruit || !fruitQuantity) {
+
+    alert("Please fill all the boxes.");
+    return;
+
+  }
+
+  let userID = user.uid;
+
+  console.log(userID);
+
+  // Save data using UID as the key
+  firebase.database().ref("fruitForms/" + userID).set({
+
+    name: name,
+    favoriteFruit: favoriteFruit,
+    fruitQuantity: fruitQuantity
+
+  });
+
+  console.log("Data sent!");
+
+  document.getElementById("statusMessage").innerText =
+    "Form submitted!";
+}
+// -----------------------------------
+// Firebase Login
+// -----------------------------------
+
+function fb_login() {
+
+  firebase.auth().onAuthStateChanged((user) => {
+
+    if (user) {
+
+      console.log("Logged in");
+      console.log(user);
+
+    } else {
+
+      console.log("Not logged in");
+
+      var provider =
+        new firebase.auth.GoogleAuthProvider();
+
+      provider.addScope('profile');
+      provider.addScope('email');
+
+      firebase.auth()
+        .signInWithPopup(provider)
+        .then(function(result) {
+
+          var token =
+            result.credential.accessToken;
+
+          var user = result.user;
+
+          console.log(user);
+
+        })
+        .catch(function(error) {
+
+          console.log(error);
+
+        });
+    }
+  });
 }
